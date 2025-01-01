1c08330a858256677ea60fe0839f2e1b012b6b6e
Aditya Saroha <143502508+Aditya-138-12@users.noreply.github.com>
2025-01-01 23:12:19 +0530

    Update firebase.js

--- src/components/FIrebase/firebase.js
+++ src/components/FIrebase/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBndS3ZAloDRXhkIKsMLcLF9sWP2rqe31g",
-    authDomain: "post-gfg-160.firebaseapp.com",
-    projectId: "post-gfg-160",
-    storageBucket: "post-gfg-160.firebasestorage.app",
-    messagingSenderId: "921779254991",
-    appId: "1:921779254991:web:ed8cb9d4274545738f29ba"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
